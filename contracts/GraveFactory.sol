dd48bfd8806c1b2174f16e31958de4d674fb7916
tsukurimono1 <tsukurimono1>
2022-02-12 23:59:11 +0900

    limits

--- contracts/GraveFactory.sol
+++ contracts/GraveFactory.sol
@@ -2,9 +2,36 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 import "../node_modules/openzeppelin-solidity/contracts/access/Ownable.sol";
+import "./Grave.sol";
 
 contract GraveFactory is Ownable {
+    mapping(address => Grave[]) private _graves;
+    uint256 constant maxLimit = 20;
+
     constructor() {
         transferOwnership(msg.sender);
+    }
+
+    function associatedGravesCount() public view returns(uint256) {
+        return _graves[msg.sender].length;
+    }
+
+    function createGrave(string memory name, int256 birth, int256 death, string memory portraitURL) public {
+        Grave grave = new Grave(name, birth, death, portraitURL, msg.sender);
+        _graves[msg.sender].push(grave);
+    }
+
+    function associatedGraves(uint256 limit, uint256 offset) public view returns(Grave[] memory coll) {
+        uint256 count = associatedGravesCount();
+        uint256 start = count - offset < 0 ? count : offset;
+        uint256 currentLimit = limit > maxLimit ? maxLimit : limit;
+        uint256 end = offset + currentLimit < count ? offset + currentLimit : count;
+
+        coll = new Grave[](end - start);
+
+        for(uint256 i=0; i+start<end; i++) {
+            coll[i] = _graves[msg.sender][i+start];
+        }
+        return coll;
     }
 }
--- test/grave_factory_test.js
+++ test/grave_factory_test.js
@@ -1,7 +1,7 @@
 const GraveFactoryContract = artifacts.require("GraveFactory");
 
 contract("GraveFactory: deployment", accounts => {
-    const owner = accounts[0]; // Note: Owner should be same as migration file.
+    const owner = accounts[0];
 
     it("has been deployed", async () => {
         const graveFactoryContract = await GraveFactoryContract.deployed();
@@ -10,9 +10,66 @@ contract("GraveFactory: deployment", accounts => {
 
     describe("initialize", () => {
         it("owner", async() => { 
-            const graveFactoryContract = await GraveFactoryContract.deployed();
+            const graveFactoryContract = await GraveFactoryContract.new({from:owner});
             const actual = await graveFactoryContract.owner();
             assert.equal(actual, owner, "owner should match");
         });
     });
+});
+
+contract("GraveFactory: operations", accounts => {
+    const name = "name";
+    const birth = -1518825600;
+    const death = 1639958400;
+    const portraitURL = "https://digital-graves/boo/bar"
+    const sender = accounts[1];
+    const owner = accounts[0];
+
+    describe("creation", () => {
+        it("associated graves count should increment by 1", async() => {
+            const graveFactoryContract = await GraveFactoryContract.new({from: owner});
+            const currentAssociatedGravesCount = await graveFactoryContract.associatedGravesCount({from: sender});
+            await graveFactoryContract.createGrave(name, birth, death, portraitURL, {from: sender});
+            const newAssociatedGravesCount = await graveFactoryContract.associatedGravesCount({from: sender});
+            assert.equal(newAssociatedGravesCount - currentAssociatedGravesCount, 1, "should increment by 1")
+        });
+    });
+
+    async function createFactory(count, graveInheritor, factoryOwner) {
+        const factory = await GraveFactoryContract.new({from: factoryOwner});
+        await addGraves(factory, count, graveInheritor);
+        return factory;
+    }
+
+    async function addGraves(factory, count, graveInheritor) {
+        for(let i=0; i<count; i++) {
+            await factory.createGrave(`${name} ${i}`, birth, death, `${portraitURL}/${i}`, {from: graveInheritor});
+        }
+    }
+
+    describe("varing limits", () => {
+        let factory;
+        const inheritor = accounts[1];
+        const factoryOwner = accounts[0];
+
+        beforeEach(async () => {
+            factory = await createFactory(20, inheritor, factoryOwner);
+        });
+
+        it("returns 10 results when limit request is 10", async() => {
+            const graves = await factory.associatedGraves(10, 0, {from: inheritor});
+            assert.equal(graves.length, 10, "results size should be 10");
+        });
+
+        it("returns 20 results when limit request is 20", async() => {
+            const graves = await factory.associatedGraves(20, 0, {from: inheritor});
+            assert.equal(graves.length, 20, "results size should be 20");
+        });
+
+        it("returns 30 results when limit request is 20", async() => {
+            const graves = await factory.associatedGraves(30, 0, {from: inheritor});
+            assert.equal(graves.length, 20, "results size should be 20");
+        });
+    });
+
 });
