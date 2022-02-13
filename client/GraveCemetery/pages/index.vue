17e150444b7ead7797b550a46dd670bdf87909ab
tsukurimono1 <tsukurimono1>
2022-02-13 20:27:23 +0900

    sample code

--- client/GraveCemetery/pages/index.vue
+++ client/GraveCemetery/pages/index.vue
@@ -5,9 +5,16 @@
 import { Component, Vue, Watch } from 'nuxt-property-decorator';
 import Web3 from "web3";
 
+import GraveFactoryContract from "../contracts/GraveFactory.json";
+
 declare global {
     interface Window {
         ethereum:any;
+    }
+
+    interface ContractObject {
+      networks:{[key:number]:any};
+      abi:any[];
     }
 }
 
@@ -19,11 +26,14 @@ export default class Index extends Vue {
   private web3:Web3|null = null
 
   async mounted() {
-    console.log(window.ethereum);
     this.web3 = new Web3(window.ethereum);
     await window.ethereum.enable();
     const accounts = await this.web3.eth.getAccounts();
-    console.log(accounts);
+    const networkId = await this.web3.eth.net.getId();
+    const object:ContractObject = GraveFactoryContract;
+    const deployedNetwork = object.networks[networkId];
+    const instance = new this.web3.eth.Contract(object.abi, deployedNetwork.address);
+    console.log(instance);
   }
 }
 </script>
