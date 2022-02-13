2c6eac6a1a4612fc8bd0d3bea41c04794bcf6460
tsukurimono1 <tsukurimono1>
2022-02-13 15:15:39 +0900

    default layout typescript

--- client/web3/layouts/default.vue
+++ client/web3/layouts/default.vue
@@ -1,86 +1,66 @@
+<i18n>
+{
+  "ja": {
+    "title": "デジタル墓地",
+    "menu": {
+      "top": "トップページ",
+      "inspire": "インスパイア"
+    }
+  },
+  "en": {
+    "title": "DigitalCemetery",
+    "menu": {
+      "top": "Toppage",
+      "inspire": "Inspire"
+    }
+  }
+}
+</i18n>
+
 <template>
   <v-app light>
     <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
+      v-model="isDrawerShown"
+      :mini-variant="isMiniVariant"
       app
     >
       <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
+        <v-list-item link exact :to="localePath({name: 'index'})" nuxt>
           <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
+            <v-icon>mdi-apps</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
+            <v-list-item-title v-text="$t('menu.top')" />
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link exact :to="localePath({name: 'inspire'})" nuxt>
+          <v-list-item-action>
+            <v-icon>mdi-chart-bubble</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="$t('menu.inspire')" />
           </v-list-item-content>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
     <v-app-bar
-      :clipped-left="clipped"
-      fixed
       app
     >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-app-bar-nav-icon @click.stop="isDrawerShown = !isDrawerShown" />
       <v-btn
         icon
-        @click.stop="miniVariant = !miniVariant"
+        @click.stop="isMiniVariant = !isMiniVariant"
       >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
+        <v-icon>mdi-{{ `chevron-${isMiniVariant ? 'right' : 'left'}` }}</v-icon>
       </v-btn>
       <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
     </v-app-bar>
     <v-main>
       <v-container>
         <Nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-footer
-      :absolute="!fixed"
       app
     >
       <span>&copy; {{ new Date().getFullYear() }}</span>
@@ -88,31 +68,15 @@
   </v-app>
 </template>
 
-<script>
-export default {
-  name: 'DefaultLayout',
-  data () {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
-    }
-  }
+<script lang="ts">
+import { Component, Vue, Watch } from 'nuxt-property-decorator';
+
+@Component({
+  components: {}
+})
+
+export default class DefaultLayout extends Vue {
+  private isDrawerShown: boolean = false
+  private isMiniVariant: boolean = false
 }
 </script>
