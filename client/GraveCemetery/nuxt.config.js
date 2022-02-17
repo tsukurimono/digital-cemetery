798917caec4525fa78eb715459cdafbcb7404655
tsukurimono1 <tsukurimono1>
2022-02-17 23:30:51 +0900

    change subdirectory

--- client/GraveCemetery/nuxt.config.js
+++ client/GraveCemetery/nuxt.config.js
@@ -3,6 +3,9 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
+  router: {
+    base: '/digital-cemetery/'
+   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
