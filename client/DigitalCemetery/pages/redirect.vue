3d988214a46b6744b77aa40cfb042a1c4d84e7a6
tsukurimono1 <tsukurimono1>
2022-02-26 10:25:12 +0900

    fix

--- client/DigitalCemetery/pages/redirect.vue
+++ client/DigitalCemetery/pages/redirect.vue
@@ -11,14 +11,20 @@ import { Component, Vue, Watch } from 'nuxt-property-decorator';
 })
 export default class Redirect extends Vue {
     mounted() {
+      console.log(this.$route.params.p);
         const query:string = typeof(this.$route.query.p) == 'string' ? this.$route.query.p : '';
+      console.log(query);
         if(this.$route.query.p == '') {
             this.$router.push('/');
         }
         const uri:string = query.replace(this.$router.options.base ?? '', '');
+      console.log(uri);
         if(this.$router.resolve(uri).resolved.matched.length > 0) {
+          console.log("before push");
             this.$router.push(uri);
+          console.log("after push");
         }
+        console.log("end of mounted");
     }
 }
 </script>
